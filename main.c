cd21f261737e64e1aafb2ee2a3fe2cb82ed34ef2
Beñat Castro <benatcastro@gmail.com>
2022-05-10 04:15:29 +0200

    makefile update

--- main.c
+++ main.c
@@ -6,18 +6,31 @@
 /*   By: bena <bena@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/26 09:39:43 by becastro          #+#    #+#             */
-/*   Updated: 2022/05/09 22:56:25 by bena             ###   ########.fr       */
+/*   Updated: 2022/05/10 04:12:50 by bena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/ft_printf.h"
 
-int	main(void)
+int main(void)
+{
+	char	*s;
+	(void)s;
+	t_args *lstargs;
+
+	lstargs = malloc(sizeof(t_args));
+
+	printf("test: %s\n", ft_putnbr_base(123, lstargs));
+	printf("\nOG: (%x)\n", 123);
+}
+
+
+int	main2(void)
 {
 	int		i;
 	char	*str;
 
-	str = "my printf, %+-06d test\n";
+	str = "my printf, %+02d test\n";
 
 	i = ft_printf(str, 123);
 	printf("i = %d\n", i);
